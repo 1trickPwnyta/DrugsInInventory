b800ca897ca9a2dee6ca702cac172a31d3a2becc
1trickPwnyta <sarahpace37@gmail.com>
2024-05-07 17:17:48 -0700

    Initial commit

---
+++ 1.5/Source/DrugsInInventoryMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace DrugsInInventory
+{
+    public class DrugsInInventoryMod : Mod
+    {
+        public const string PACKAGE_ID = "drugsininventory.1trickPwnyta";
+        public const string PACKAGE_NAME = "Drugs in Inventory";
+
+        public DrugsInInventoryMod(ModContentPack content) : base(content)
+        {
+            Harmony harmony = new Harmony(PACKAGE_ID);
+            harmony.PatchAll();
+
+            Log.Message($"[{PACKAGE_NAME}] Loaded.");
+        }
+    }
+}
---
+++ 1.5/Source/Patch_Pawn_DrugPolicyTracker.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace DrugsInInventory
+{
+    [HarmonyPatch(typeof(Pawn_DrugPolicyTracker))]
+    [HarmonyPatch(nameof(Pawn_DrugPolicyTracker.AllowedToTakeToInventory))]
+    public static class Patch_Pawn_DrugPolicyTracker
+    {
+        public static bool Prefix(Pawn_DrugPolicyTracker __instance, ThingDef thingDef, ref bool __result)
+        {
+            if (!thingDef.IsIngestible)
+            {
+                Log.Error(thingDef + " is not ingestible.");
+                __result = false;
+            }
+            else if (!thingDef.IsDrug)
+            {
+                Log.Error("AllowedToTakeScheduledEver on non-drug " + thingDef);
+                __result = false;
+            }
+            else if (thingDef.IsNonMedicalDrug && !__instance.pawn.CanTakeDrug(thingDef))
+            {
+                __result = false;
+            }
+            else
+            {
+                DrugPolicyEntry drugPolicyEntry = __instance.CurrentPolicy[thingDef];
+                __result = __instance.pawn.inventory.innerContainer.TotalStackCountOfDef(thingDef) < drugPolicyEntry.takeToInventory;
+            }
+
+            return false;
+        }
+    }
+}
